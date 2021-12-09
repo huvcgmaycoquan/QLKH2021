f569351432838551be64145e1bab0501a06a6439
trieunv <ngvtrieusqtt@gmail.com>
2021-12-09 20:41:22 +0700

    t

---
+++ QLKH2021/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLKH2021
+{
+    class Class1
+    {
+        public int dfllfkfj { get; set; }
+    }
+}
